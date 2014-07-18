ea0d67c91ad2eb235319a91d8835be1e4ffd6d3e
sufex00 <pedro_h_nr@hotmail.com>
2014-07-18 17:51:18 -0300

    limpeza

--- testeCabecalho/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <QDebug>
-#include"treatmentheader.h"
-
-int main(int argc, char *argv[])
-{
-    TreatmentHeader teste = TreatmentHeader(argc,argv);
-    qDebug()<<"input:"<<teste.getFileInput();
-    qDebug()<<"output:"<<teste.getFileOutput().append("pedrobarro").toLocal8Bit().data();
-}
